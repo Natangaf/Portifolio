75b6daa76ae03c5e125ebbacaed4fe374c732c45
natabgaf <natanfernandes52@gmail.com>
2023-04-24 22:17:18 -0300

    fat carrocvl

--- src/components/cardProject/index.tsx
+++ src/components/cardProject/index.tsx
@@ -1,12 +1,13 @@
-import { useContext, ReactElement } from "react"
+import { useContext, ReactElement, useState } from "react"
 import { useNavigate } from "react-router-dom";
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
 import { StylesCardProject } from "./style";
 import { StyledTypography } from "../baseTypography/style";
 import { StyledButtons } from "../../styles/Buttons";
 import { ProjectsList } from "../../base/BaseProjects";
 import { ProjectContext } from "../../context/ProjectContext";
 import { ImgLanguages } from './../imgLanguages/imgLanguages';
-import { AliceCarousel } from 'react-alice-carousel';
 
 
 
@@ -47,19 +48,83 @@ export function CardProject() {
             </ StylesCardProject>
         )
     )
+    const thumbItems = (items: any, [setThumbIndex, , setThumbAnimation]: any,) => {
+        return items.map((item: any, i: number,) => (
+            <div className="thumb" onClick={() => (setThumbIndex(i), setThumbAnimation(true))}>
+                {item}
+            </div>
+        ));
+    };
+    const [mainIndex, setMainIndex] = useState(0);
+    const [mainAnimation, setMainAnimation] = useState(false);
+    const [thumbIndex, setThumbIndex] = useState(0);
+    const [thumbAnimation, setThumbAnimation] = useState(false);
+    const [thumbs] = useState(thumbItems(productElement, [setThumbIndex, setThumbAnimation]));
 
-    return (
-        <AliceCarousel 
-            mouseTracking
-            touchTracking
-            items={productElement}
-            controlsStrategy="alternate"
+    const slideNext = () => {
+        if (!thumbAnimation && thumbIndex < thumbs.length - 1) {
+            setThumbAnimation(true);
+            setThumbIndex(thumbIndex + 1);
+        }
+    };
+
+    const slidePrev = () => {
+        if (!thumbAnimation && thumbIndex > 0) {
+            setThumbAnimation(true);
+            setThumbIndex(thumbIndex - 1);
+        }
+    };
+
+    const syncMainBeforeChange = (e: any) => {
+        setMainAnimation(true);
+    };
+
+    const syncMainAfterChange = (e: any) => {
+        setMainAnimation(false);
+
+        if (e.type === 'action') {
+            setThumbIndex(e.item);
+            setThumbAnimation(false);
+        } else {
+            setMainIndex(thumbIndex);
+        }
+    };
+
+    const syncThumbs = (e: any) => {
+        setThumbIndex(e.item);
+        setThumbAnimation(false);
+
+        if (!mainAnimation) {
+            setMainIndex(e.item);
+        }
+    };
+
+    return [
+        <AliceCarousel
+            activeIndex={mainIndex}
+            animationType="fadeout"
+            animationDuration={800}
+            disableDotsControls
             disableButtonsControls
-            animationDuration={7000}
-            autoPlay
-            paddingLeft={0}
-            autoPlayStrategy="none"
-            infinite
-        />
-    )
+            items={productElement}
+            mouseTracking={!thumbAnimation}
+            onSlideChange={syncMainBeforeChange}
+            onSlideChanged={syncMainAfterChange}
+            touchTracking={!thumbAnimation}
+        />,
+        <div className="thumbs">
+            <AliceCarousel
+                activeIndex={thumbIndex}
+                autoWidth
+                disableDotsControls
+                disableButtonsControls
+                items={thumbs}
+                mouseTracking={false}
+                onSlideChanged={syncThumbs}
+                touchTracking={!mainAnimation}
+            />
+            <div className="btn-prev" onClick={slidePrev}>&lang;</div>
+            <div className="btn-next" onClick={slideNext}>&rang;</div>
+        </div>
+    ]
 }
