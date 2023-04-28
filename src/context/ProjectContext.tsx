c2eda6c47a85fb4928447dacb74c1636fb734cac
natabgaf <natanfernandes52@gmail.com>
2023-04-28 15:34:36 -0300

    mrnu mobile completed

--- src/context/ProjectContext.tsx
+++ src/context/ProjectContext.tsx
@@ -12,38 +12,38 @@ export const ProjectProvider = ({ children }: iProjecProviderProps) => {
 
 
     const slide = {
-        // mostrar: {
-        //     y: 0,
-        // },
-        // esconder: {
-        //     y: "-100%",
-        // },
-        // inicial: {
-        //     y: "100%",
-        // },
+        mostrar: {
+            y: 0,
+        },
+        esconder: {
+            y: "-100%",
+        },
+        inicial: {
+            y: "100%",
+        },
     }
 
     const pages = ["/", "/About", "/Projects", "/Education", "/Contact"]
 
     const handleScroll = () => {
-        // const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-        // const currentPageIndex = pages.findIndex(page => page === location.pathname);
+        const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+        const currentPageIndex = pages.findIndex(page => page === location.pathname);
 
-        // if (scrollTop + clientHeight >= scrollHeight) {
-        //     const nextPageIndex = currentPageIndex + 1;
-        //     const nextPage = pages[nextPageIndex];
-        //     if (nextPage !== location.pathname && nextPage !== undefined && location.pathname !== "/Project") {
-        //         navigate(nextPage);
-        //         window.scrollTo(0, 1);
-        //     }
-        // } else if (scrollTop === 0) {
-        //     const prevPageIndex = currentPageIndex - 1;
-        //     const prevPage = pages[prevPageIndex];
-        //     if (prevPage !== location.pathname && prevPage !== undefined && location.pathname !== "/Project") {
-        //         navigate(prevPage);
-        //         window.scrollTo(0, 1);
-        //     }
-        // }
+        if (scrollTop + clientHeight >= scrollHeight) {
+            const nextPageIndex = currentPageIndex + 1;
+            const nextPage = pages[nextPageIndex];
+            if (nextPage !== location.pathname && nextPage !== undefined && location.pathname !== "/Project") {
+                navigate(nextPage);
+                window.scrollTo(0, 1);
+            }
+        } else if (scrollTop === 0) {
+            const prevPageIndex = currentPageIndex - 1;
+            const prevPage = pages[prevPageIndex];
+            if (prevPage !== location.pathname && prevPage !== undefined && location.pathname !== "/Project") {
+                navigate(prevPage);
+                window.scrollTo(0, 1);
+            }
+        }
     };
 
     useEffect(() => {
