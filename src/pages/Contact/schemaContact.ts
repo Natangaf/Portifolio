cc9c05246dace7e7373c871430d36673a58a5ac7
natabgaf <natanfernandes52@gmail.com>
2023-07-21 14:31:56 -0300

    fex bugas

--- src/pages/Contact/schemaContact.ts
+++ src/pages/Contact/schemaContact.ts
@@ -1,7 +1,8 @@
 import * as yup from "yup";
+import { FormValues } from "../../interface";
 
 export const schemaContact = yup.object().shape({
     name: yup.string().required("Como Vou saber Quem é você,me conta ai"),
     email: yup.string().required("Sem seu e-mail nos não vamos conseguir conversar").email("É necessário fornecer um email válido"),
     message: yup.string().required("Me conta sobre o que vamos Conversar")
-})
+}) as yup.ObjectSchema<FormValues, object>;
