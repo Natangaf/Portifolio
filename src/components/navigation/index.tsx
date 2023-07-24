83948ca1f3780bb934aabc96774a589757dd8485
natabgaf <natanfernandes52@gmail.com>
2023-07-24 14:42:20 -0300

    header

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -12,14 +12,14 @@ interface MenuItem {
     slideIndex: number;
 }
 
-const MenuButton = ({ ctx, item }: { ctx: Slide; item: MenuItem }) => {
+const MenuButton = ({ ctx, item, setIsOpen }: { ctx: Slide; item: MenuItem; setIsOpen: (isOpen: boolean) => void }) => {
     const handleMenuClick = () => {
         ctx.goto(ctx.slides[item.slideIndex], true);
+        setIsOpen(false);
     };
 
     return (
         <button
-            key={item.slideIndex}
             className={ctx.number === item.slideIndex ? 'selected' : ''}
             onClick={handleMenuClick}
         >
@@ -28,18 +28,18 @@ const MenuButton = ({ ctx, item }: { ctx: Slide; item: MenuItem }) => {
     );
 };
 
+const menuItems: MenuItem[] = [
+    { label: 'HOME', slideIndex: 0 },
+    { label: 'SOBRE', slideIndex: 1 },
+    { label: 'PROJETOS', slideIndex: 2 },
+    { label: 'EDUCAÇÃO', slideIndex: 3 },
+    { label: 'CONTATO', slideIndex: 4 },
+];
+
 export function Navigation() {
     const [isOpen, setIsOpen] = useState(false);
     const modalRef = useOutClick(() => setIsOpen(false));
     const location = useLocation();
-
-    const menuItems: MenuItem[] = [
-        { label: 'HOME', slideIndex: 0 },
-        { label: 'SOBRE', slideIndex: 1 },
-        { label: 'PROJETOS', slideIndex: 2 },
-        { label: 'EDUCAÇÃO', slideIndex: 3 },
-        { label: 'CONTATO', slideIndex: 4 },
-    ];
 
     return (
         <StylesNav>
@@ -47,7 +47,9 @@ export function Navigation() {
                 <>
                     <div className="navigation">
                         <FullpageContext.Consumer>
-                            {(ctx: Slide) => menuItems.map((item) => <MenuButton key={item.slideIndex} ctx={ctx} item={item} />)}
+                            {(ctx: Slide) => menuItems.map((item) => (
+                                <MenuButton key={item.slideIndex} ctx={ctx} item={item} setIsOpen={setIsOpen} />
+                            ))}
                         </FullpageContext.Consumer>
                     </div>
                     <button className="bntMenu" onClick={() => setIsOpen(true)}>
@@ -68,9 +70,11 @@ export function Navigation() {
                                 <button onClick={() => setIsOpen(!isOpen)} className="bntMenu">
                                     <img src={closeMenu} alt="" />
                                 </button>
-                                <FullpageContext.Consumer>
-                                    {(ctx: Slide) => menuItems.map((item, index) => <MenuButton key={index} ctx={ctx} item={item} />)}
-                                </FullpageContext.Consumer>
+                                {menuItems.map((item, index) => (
+                                    <FullpageContext.Consumer key={index}>
+                                        {(ctx: Slide) => <MenuButton ctx={ctx} item={item} setIsOpen={setIsOpen} />}
+                                    </FullpageContext.Consumer>
+                                ))}
                             </motion.nav>
                         )}
                     </AnimatePresence>
